2e7980a47a876bda3375352aa6729d8b572dcb5e
peu015 pestana <pietropestana@gmail.com>
2024-06-20 00:15:51 +0000

    criação do helper

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
-let tema = "claro"
+import { trocarTema, verificarTema } from "./helpers/tema-helper.js"
 
 const body = document.querySelector("body")
-
 const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventListener("click", trocarTema)
 
-function trocarTema(){
-    if (localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
+botaoTema.addEventListener("click", () => {
 
-    const body = document.querySelector("body")
-
-    if(tema === "claro") {
-        body.classList.add("escuro")
-        localStorage.setItem("tema", "escuro")
-    }else{
-        body.classList.remove("escuro")
-        localStorage.setItem("tema", "claro")
-    }
+    trocarTema(body, botaoTema)
 }
+)
 
-function verificarTema(){
-    if(localStorage.getItem("tema")) {
-        tema = localStorage.getItem("tema")
-    }
-
-    if(tema === "escuro"){
-        body.classList.add("escuro")
-    }
-}
-
-verificarTema()
+verificarTema(body, botaoTema)
