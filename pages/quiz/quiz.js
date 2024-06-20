c585ffc394cde81815645c3a97dc12217f063200
peu015 pestana <pietropestana@gmail.com>
2024-06-20 01:08:57 +0000

    alteração direta do assunto do quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -10,3 +10,20 @@ botaoTema.addEventListener("click", () => {
 )
 
 verificarTema(body, botaoTema)
+
+const assunto = localStorage.getItem("assunto")
+
+
+
+function alterarAssunto(){
+    const divIcone = document.querySelector(".assunto_icone ")
+    const img = document.querySelector(".assunto_icone img")
+    const titulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    img.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    img.setAttribute("alt", `Icone de ${assunto.toLowerCase()}`)
+    titulo.innerText = assunto
+}
+
+alterarAssunto()
