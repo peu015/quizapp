a8dad3881a73132010599acbfe9536077eeb281e
peu015 pestana <pietropestana@gmail.com>
2024-06-20 03:18:58 +0000

    fim

--- pages/resultado/resultado.js
+++ pages/resultado/resultado.js
@@ -2,6 +2,8 @@ import { trocarTema, verificarTema } from "../../helpers/tema-helper.js"
 
 const body = document.querySelector("body")
 const botaoTema = document.querySelector(".tema button")
+const assunto = localStorage.getItem("assunto")
+const botaoJogar = document.querySelector("main button")
 
 botaoTema.addEventListener("click", () => {
 
@@ -9,4 +11,44 @@ botaoTema.addEventListener("click", () => {
 }
 )
 
+botaoJogar = addEventListener("click", jogarNovamente)
+
 verificarTema(body, botaoTema)
+
+function alterarAssunto(){
+    const divIcone = document.querySelector(".assunto_icone ")
+    const img = document.querySelector(".assunto_icone img")
+    const titulo = document.querySelector(".assunto h1")
+
+    divIcone.classList.add(assunto.toLowerCase())
+    img.setAttribute("src", `../../assets/images/icon-${assunto.toLowerCase()}.svg`)
+    img.setAttribute("alt", `Icone de ${assunto.toLowerCase()}`)
+    titulo.innerText = assunto
+}
+
+alterarAssunto()
+
+function inserirResultado(){
+    const sectionPontuacao = document.querySelector(".pontuacao")
+    const divAssunto = document.querySelector(".assunto")
+    const pontos = localStorage.getItem("pontos")
+
+    sectionPontuacao.innerHTML = `
+        ${divAssunto.outerHTML} 
+            <strong>${pontos}</strong>
+
+                <p>de 10</p>
+    
+
+    `
+}
+
+function jogarNovamente(){
+    localStorage.removeItem("pontos")
+    localStorage.removeItem("assunto")
+
+    window.location.href="../../index.html"
+}
+
+inserirResultado()
+
